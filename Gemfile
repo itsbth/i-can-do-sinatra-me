3f9752e9009c053165826c8e22849eabd71a344e
Bjørn Tore Håvie <itsbth@itsbth.com>
2013-08-10 01:41:25 +0200

    Eet werks

--- Gemfile
+++ Gemfile
@@ -3,3 +3,7 @@ source "https://rubygems.org"
 
 gem 'sinatra'
 gem 'slim'
+
+
+gem 'sinatra-sequel'
+gem 'pg'
--- web.rb
+++ web.rb
@@ -1,6 +1,26 @@
 require 'sinatra'
 require 'slim'
+require 'sinatra/sequel'
+
+set :views, File.dirname(__FILE__) + '/views'
+set :database, ENV['DATABASE_URL'] || 'postgres://localhost/icdsm'
+
+migration "create user table" do
+  database.create_table :users do
+    primary_key :id
+    String :wow_name
+    String :lol_name
+  end
+end
+
+class User < Sequel::Model
+end
 
 get '/' do
-  "I CAN DO WEB, ME"
+  slim :index, locals: {users: User.all}
+end
+
+post '/register' do
+  User.create(params)
+  redirect '/'
 end
